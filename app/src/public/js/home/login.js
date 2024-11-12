04e734af325f050807d4f9ce11215a2e374c569b
신휘서 <hwiseo@sinhwiseoui-MacBookAir.local>
2024-11-12 23:54:16 +0900

    로그인을 요청하는 fetch 생성

---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        password : password.value 
+    };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(req),
+    });
+}
